51a4295dcc3966bb64cebf1484db8a71ef2b9a15
Aeron <aerontarroza5@gmail.com>
2024-12-15 14:27:57 +0800

    reconfig vite

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import commonjs from '@rollup/plugin-commonjs';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    commonjs(), // Add CommonJS support
-  ],
-  resolve: {
-    alias: {
-      jquery: 'jquery/dist/jquery.js', // Explicit alias for jQuery
-    },
-  },
+  plugins: [react()],
   define: {
     'global': 'window',
   },
-});
+  base: '/'
+})
